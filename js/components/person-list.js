9d50fdf94efb118e6bd1524e753cd79def766ecb
Brian Barrow <briancbarrow@gmail.com>
2016-11-16 06:18:22 -0700

    second assignment complete

---
+++ js/components/person-list.js
@@ -0,0 +1,18 @@
+// js/components/person-list.js
+var Person = require('./person');
+var React = require('react');
+
+var PersonList = function() {
+    return (
+        <div className="person-list">
+            <Person name="Derek Zoolander"
+                    imageUrl="http://uifaces.com/assets/static/images/zoolander.jpg"
+                    job="Male model" />
+            <Person name="Donald Knuth"
+                    imageUrl="http://www-cs-faculty.stanford.edu/~uno/don.gif"
+                    job="Clever chap" />
+        </div>
+    );
+};
+
+module.exports = PersonList;
--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 require('babel-polyfill');
-
+var React = require('react');
 var ReactDOM = require('react-dom');
-var Person = require('./components/person');
+// var Person = require('./components/person');
+var PersonList = require('./components/person-list');
 
 document.addEventListener('DOMContentLoaded', function() {
-    ReactDOM.render(<Person />, document.getElementById('app'));
+    ReactDOM.render(<PersonList />, document.getElementById('app'));
 });
